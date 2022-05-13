34aef858b1b401075bbacdfe5c5fde454e0ec457
WithLithum <WithLithum@foxmail.com>
2022-05-13 12:18:09 +0800

    ✨ Add process util

---
+++ NexusKrop.Bohrium/ProcessUtil.cs
@@ -0,0 +1,25 @@
+namespace NexusKrop.Bohrium;
+using System.Diagnostics;
+
+/// <summary>
+/// Provides methods to manipulate and create processes.
+/// </summary>
+public static class ProcessUtil
+{
+    /// <summary>
+    /// Opens the specified file via the default program associated with the type of such
+    /// file. If such file is an executable, starts that executable. 
+    /// </summary>
+    /// <param name="fileName">The name (or path to) the file to open.</param>
+    /// <returns><inheritdoc href="Process.Start(string)"/></returns>
+    public static Process? Execute(string fileName)
+    {
+        var info = new ProcessStartInfo()
+        {
+            UseShellExecute = true,
+            FileName = fileName
+        };
+
+        return Process.Start(info);
+    }
+}
