8b743634a482f1d363c58ff3ca2d5892bc9854df
WithLithum <WithLithum@foxmail.com>
2022-05-13 11:50:24 +0800

    :tada: Initial commit public

---
+++ NexusKrop.Bohrium.Standard/RandomUtil.cs
@@ -0,0 +1,28 @@
+﻿namespace NexusKrop.Borhium;
+using System;
+
+/// <summary>
+/// Provides methods for pseudo-random number generation.
+/// </summary>
+public static class RandomUtil
+{
+    private static readonly Random _random = new();
+
+    /// <inheritdoc cref="Random.Next"/>
+    public static int GetInt()
+    {
+        return _random.Next();
+    }
+
+    /// <inheritdoc cref="Random.Next(int)"/>
+    public static int GetInt(int maxValue)
+    {
+        return _random.Next(maxValue);
+    }
+
+    /// <inheritdoc cref="Random.Next(int, int)"/>
+    public static int GetInt(int minValue, int maxValue)
+    {
+        return _random.Next(minValue, maxValue);
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Bohrium
+
+This is the common library used by our .NET projects.
+
+## Structure
+
+All stuff that can be used on any .NET implementation is in `Bohrium.Standard`.
+All others are in the `Bohrium` project.
+
+## Usage
+
+This library does not do much on it's own. We usually refer this by submodules or NuGet package from our own feed if possible.
+
+Keep in mind that this is STILL under development.
