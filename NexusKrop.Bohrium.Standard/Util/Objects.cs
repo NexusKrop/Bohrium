0d609a4882313b7ebc81d223b85c0bf86206c1bf
WithLithum <WithLithum@foxmail.com>
2022-05-21 17:17:59 +0800

    ✨ Require argument non-nullgp

---
+++ NexusKrop.Bohrium.Standard/Util/Objects.cs
@@ -0,0 +1,27 @@
+namespace NexusKrop.Bohrium.Util;
+
+using System;
+
+/// <summary>
+/// Provides utilities to check, manipulate and monitor objects.
+/// </summary>
+public static class Objects
+{
+    /// <summary>
+    /// Requires an argument not <see langword="null"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the <paramref name="arg"/>.</typeparam>
+    /// <param name="arg">The argument.</param>
+    /// <param name="name">The name of the <paramref name="arg"/> in the calling method/</param>
+    /// <returns>The value in <paramref name="arg"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="arg"/> was null.</exception>
+    public static T RequiresArgNonNull<T>(T arg, string name)
+    {
+        if (arg == null)
+        {
+            throw new ArgumentNullException(name);
+        }
+
+        return arg;
+    }
+}
