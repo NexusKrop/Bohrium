a6e5a3494fa6e2d58ae3e41ab40e8190db9e4665
WithLithum <WithLithum@foxmail.com>
2022-05-21 18:18:16 +0800

    :white_check_mark: Add resource location test
    And the test project.

---
+++ NexusKrop.Borhium.Tests/ResourceLocationTests.cs
@@ -0,0 +1,68 @@
+namespace NexusKrop.Borhium.Tests;
+
+using NexusKrop.Bohrium.Resource;
+
+[TestClass]
+public class ResourceLocationTests
+{
+    [TestMethod]
+    public void ValidValueTest()
+    {
+        try
+        {
+            _ = new ResourceLocation("bohrium", "tests/test1");
+        }
+        catch
+        {
+            Assert.Fail();
+        }
+    }
+
+    [TestMethod]
+    public void InvalidValueTest()
+    {
+        try
+        {
+            _ = new ResourceLocation("bohr@ium","tests/test2@@");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex);
+            return;
+        }
+
+        Assert.Fail("Does not found invalid character");
+    }
+
+    [TestMethod]
+    public void InvalidNamespaceTest()
+    {
+        try
+        {
+            _ = new ResourceLocation("bohr@ium", "tests/test3");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex);
+            return;
+        }
+
+        Assert.Fail("Does not found invalid character in name-space");
+    }
+
+    [TestMethod]
+    public void InvalidKeyTest()
+    {
+        try
+        {
+            _ = new ResourceLocation("bohrium", "tests/test4@@");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex);
+            return;
+        }
+
+        Assert.Fail("Does not found invalid character in key");
+    }
+}
