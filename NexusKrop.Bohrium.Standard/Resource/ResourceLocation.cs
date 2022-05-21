6ef17b5de3fb258e14f64271ce0d95df46bdf7a9
WithLithum <WithLithum@foxmail.com>
2022-05-21 18:03:23 +0800

    :sparkles: Resource location support
    And introduce en-GB for spell checking

--- NexusKrop.Bohrium.Standard/Resource/ResourceLocation.cs
+++ NexusKrop.Bohrium.Standard/Resource/ResourceLocation.cs
@@ -7,14 +7,22 @@ using System;
 using System.Text.RegularExpressions;
 using NexusKrop.Bohrium.Util;
 
+/// <summary>
+/// Represents a resource location.
+/// </summary>
 public class ResourceLocation
 {
     private string _ns;
     private string _key;
 
-    private static readonly Regex namespaceRegex = new Regex("^[a-z0-9_.-]+$");
-    private static readonly Regex keyRegex = new Regex("^[a-z0-9_.-/]+$");
+    private static readonly Regex namespaceRegex = new("^[a-z0-9_.-]+$");
+    private static readonly Regex keyRegex = new("^[a-z0-9_.-/]+$");
 
+    /// <summary>
+    /// Initialises a new instance of the <see cref="ResourceLocation"/> class.
+    /// </summary>
+    /// <param name="ns"></param>
+    /// <param name="key"></param>
     public ResourceLocation(string ns, string key)
     {
         Namespace = Objects.RequiresArgNonNull(ns, nameof(ns));
@@ -25,7 +33,7 @@ public class ResourceLocation
     {
         if (!namespaceRegex.IsMatch(ns))
         {
-            throw new FormatException($"Invalid resource location namespace: {ns}");
+            throw new FormatException($"Invalid resource location name-space: {ns}");
         }
     }
 
@@ -37,6 +45,18 @@ public class ResourceLocation
         }
     }
 
+    /// <summary>
+    /// Returns the string representation of this resource location identifier.
+    /// </summary>
+    /// <remarks>
+    /// String representations of resource location identifiers looks like <c>namespace:key</c>.
+    /// </remarks>
+    /// <returns>The string representation of this resource location identifier.</returns>
+    public override string ToString()
+    {
+        return $"{Namespace}:{Key}";
+    }
+
     public string Namespace
     {
         get => _ns;
@@ -46,5 +66,13 @@ public class ResourceLocation
             _ns = value;
         }
     }
-    public string Key { get; set; }
+    public string Key
+    {
+        get => _key;
+        set
+        {
+            VerifyKey(value);
+            _key = value;
+        }
+    }
 }
