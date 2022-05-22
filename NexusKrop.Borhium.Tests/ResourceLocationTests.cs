d6cecaee942062480711c7f7eac96f00683c16e0
WithLithum <WithLithum@foxmail.com>
2022-05-22 15:16:59 +0800

    :white_check_mark: Added tests for new ResourceLocaton features
    Also for ResourceUtil.

--- NexusKrop.Borhium.Tests/ResourceLocationTests.cs
+++ NexusKrop.Borhium.Tests/ResourceLocationTests.cs
@@ -68,4 +68,61 @@ public class ResourceLocationTests
 
         Assert.Fail("Does not found invalid character in key");
     }
+
+    [TestMethod]
+    public void StringToLocationTest()
+    {
+        var rl = ResourceLocation.Parse("bohrium:tests/test5");
+        Assert.AreEqual("bohrium", rl.Namespace);
+        Assert.AreEqual("tests/test5", rl.Key);
+    }
+
+    [TestMethod]
+    public void LocationToStringTest()
+    {
+        var str = new ResourceLocation("bohrium", "tests/test6").ToString();
+        Assert.AreEqual("bohrium:tests/test6", str);
+    }
+
+    [TestMethod]
+    public void LooseResourcePathTest()
+    {
+        var path = ResourceUtil.GetResourcePath(new ResourceLocation("bohrium", "tests/test7"));
+        var verify = $"bohrium{Path.DirectorySeparatorChar}tests{Path.DirectorySeparatorChar}test7";
+
+        Assert.AreEqual(verify, path);
+    }
+
+    [TestMethod]
+    public void StrictResourcePathFunctionTest()
+    {
+        var path = ResourceUtil.GetResourcePathStrict(new ResourceLocation("bohrium", "tests/test8"));
+        var verify = $"bohrium{Path.DirectorySeparatorChar}tests{Path.DirectorySeparatorChar}test8";
+
+        Assert.AreEqual(verify, path);
+    }
+
+    [TestMethod]
+    public void StrictResourcePathVerifyTest()
+    {
+        try
+        {
+            _ = ResourceUtil.GetResourcePathStrict(new ResourceLocation("bohrium", "tests.test9"));
+        }
+        catch (ArgumentException aex)
+        {
+            Console.WriteLine(aex);
+            return;
+        }
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void LooseResourcePathFixTest()
+    {
+        var path = ResourceUtil.GetResourcePath(new ResourceLocation("bohrium", "tests.test10"));
+        var verify = $"bohrium{Path.DirectorySeparatorChar}tests{Path.DirectorySeparatorChar}test10";
+
+        Assert.AreEqual(verify, path);
+    }
 }
