9bdf87cb52306dd963f9ebfa0af93d47639ed009
WithLithum <WithLithum@foxmail.com>
2022-05-25 18:56:51 +0800

    :sparkles: String extensions
    And to snake case method. Awesome?
    Copied from https://gist.github.com/vkobel/d7302c0076c64c95ef4b
    
    Intended usage tested, test included

---
+++ NexusKrop.Bohrium.Standard/Util/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿// Bohrium for .NET Standard (Mono/.NET Framework/.NET/...)
+// (C) NexusKrop & contributors 2022. Licensed under Apache-2.0 licence.
+// See https://github.com/NexusKrop/Bohrium/LICENSE
+
+namespace NexusKrop.Bohrium.Util;
+using System.Linq;
+
+/// <summary>
+/// Provides methods to manipulate strings.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Converts a <c>PascalCase</c> or <c>camelCase</c> string to a snake case string.
+    /// </summary>
+    /// <param name="str">The string to convert.</param>
+    /// <returns>The converted string.</returns>
+    public static string ToSnakeCase(this string str)
+    {
+        return string.Concat(
+            str.Select(
+                (x, i) => i > 0 && char.IsUpper(x)
+                    ? "_" + x
+                    : x.ToString()
+                    )
+            ).ToLower();
+    }
+}
---
+++ NexusKrop.Borhium.Tests/StringExtensionTests.cs
@@ -0,0 +1,20 @@
+﻿namespace NexusKrop.Borhium.Tests;
+using NexusKrop.Bohrium.Util;
+
+[TestClass]
+public class StringExtensionTests
+{
+    public const string ExceptedSnakeCaseResult = "snake_case_test";
+
+    [TestMethod]
+    public void PascalToSnakeTest()
+    {
+        Assert.AreEqual(ExceptedSnakeCaseResult, "SnakeCaseTest".ToSnakeCase());
+    }
+
+    [TestMethod]
+    public void CamelToSnakeTest()
+    {
+        Assert.AreEqual(ExceptedSnakeCaseResult, "snakeCaseTest".ToSnakeCase());
+    }
+}
