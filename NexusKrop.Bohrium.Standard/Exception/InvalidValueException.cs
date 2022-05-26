76b11f1571467b539e50149cd989e6d9957335b9
WithLithum <WithLithum@foxmail.com>
2022-05-26 09:39:05 +0800

    ✨ Add property value related exceptions
    Probably standardise on this exception for all property values!

---
+++ NexusKrop.Bohrium.Standard/Exception/InvalidValueException.cs
@@ -0,0 +1,35 @@
+﻿namespace NexusKrop.Bohrium.Exception;
+using System;
+
+/// <summary>
+/// The exception that is thrown when a <see langword="null"/> reference (<c>Nothing</c> in Visual Basic) is passed to the setter of
+/// a property that does not accept it as a valid value.
+/// </summary>
+[Serializable]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S3925:\"ISerializable\" should be implemented correctly", Justification = "<Pending>")]
+public class InvalidValueException : Exception
+{
+    /// <summary>
+    /// Initialises a new instance of the <see cref="InvalidValueException"/> class.
+    /// </summary>
+    public InvalidValueException() : base(ExceptionResources.InvalidValueException)
+    {
+    }
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="InvalidValueException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    public InvalidValueException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="InvalidValueException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="innerException">The inner exception.</param>
+    public InvalidValueException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ NexusKrop.Bohrium.Standard/Exception/ValueNullException.cs
@@ -0,0 +1,35 @@
+﻿namespace NexusKrop.Bohrium.Exception;
+using System;
+
+/// <summary>
+/// The exception that is thrown when a <see langword="null"/> reference (<c>Nothing</c> in Visual Basic) is passed to the setter of
+/// a property that does not accept it as a valid value.
+/// </summary>
+[Serializable]
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S3925:\"ISerializable\" should be implemented correctly", Justification = "<Pending>")]
+public class ValueNullException : Exception
+{
+    /// <summary>
+    /// Initialises a new instance of the <see cref="ValueNullException"/> class.
+    /// </summary>
+    public ValueNullException() : base(ExceptionResources.NullValueException)
+    {
+    }
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="ValueNullException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    public ValueNullException(string message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="ValueNullException"/> class.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    /// <param name="innerException">The inner exception.</param>
+    public ValueNullException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
