09ad130dd7b691e7a7242472b96baf8a6ed61241
WithLithum <WithLithum@foxmail.com>
2022-06-10 11:33:02 +0800

    ✨ TimeUtil

---
+++ NexusKrop.Bohrium.Standard/Util/TimeUtil.cs
@@ -0,0 +1,20 @@
+﻿namespace NexusKrop.Bohrium.Util;
+using System;
+
+/// <summary>
+/// Provides utilities to manipulate date and time.
+/// </summary>
+public static class TimeUtil
+{
+    public static readonly DateTime UnixEpoch = new(1970, 1, 1, 0, 0, 0);
+
+    /// <summary>
+    /// Converts the Unix-epoch based time to the .NET time system.
+    /// </summary>
+    /// <param name="source">The source.</param>
+    /// <returns>An instance of <see cref="DateTime"/> representing the <paramref name="source"/> as .NET time.</returns>
+    public static DateTime FromEpochTime(long source)
+    {
+        return UnixEpoch.AddMilliseconds(source);
+    }
+}
