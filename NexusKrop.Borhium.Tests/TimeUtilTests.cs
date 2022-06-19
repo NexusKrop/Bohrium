b83d221908c27c9aac0d908fcbf46f4efe88ae63
WithLithum <WithLithum@foxmail.com>
2022-06-19 12:44:34 +0800

    :white_check_mark: Add ToUnixEpochTimeTest

--- NexusKrop.Borhium.Tests/TimeUtilTests.cs
+++ NexusKrop.Borhium.Tests/TimeUtilTests.cs
@@ -13,11 +13,22 @@ public class TimeUtilTests
     [TestMethod]
     public void FromUnixEpochTimeTest()
     {
-        const long fromTime = 1655612168L;
+        const long fromTime = 1655583368L;
         var dateTime = new DateTime(2022, 06, 19, 04, 16, 08);
 
         var actual = TimeUtil.FromEpochTime(fromTime);
         Console.WriteLine(actual);
         Assert.AreEqual(dateTime, actual);
+    }
+
+    [TestMethod]
+    public void ToUnixEpochTimeTest()
+    {
+        const long epochTime = 1655583368L;
+        var dateTime = new DateTime(2022, 06, 19, 04, 16, 08);
+
+        var actual = TimeUtil.ToEpochTime(dateTime);
+        Console.WriteLine(actual);
+        Assert.AreEqual(epochTime, actual);
     }
 }
