d7a218d9f8104de1f41c905ea20dfcd7c3265ba1
WithLithum <WithLithum@foxmail.com>
2022-06-22 09:34:39 +0800

    ✅ Add BAC unit conversion tests
    resolves BOH-3

---
+++ NexusKrop.Borhium.Tests/UnitConversionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace NexusKrop.Borhium.Tests;
+
+using NexusKrop.Bohrium.Util;
+
+[TestClass]
+public class UnitConversionTests
+{
+    [TestMethod]
+    public void BacToMilliTest()
+    {
+        Assert.AreEqual(80, UnitConversions.BacToMilligrams(0.08));
+        Assert.AreEqual(80f, UnitConversions.BacToMilligrams(0.08f));
+    }
+
+    [TestMethod]
+    public void MilliToBacTest()
+    {
+        Assert.AreEqual(0.08, UnitConversions.MilligramsToBac(80d));
+        Assert.AreEqual(0.08f, UnitConversions.MilligramsToBac(80f));
+    }
+}
